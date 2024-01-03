40bdc953250cc7ea55abfcf72bcedd8111dbc0ec
Elie Taillard <taillardelie@gmail.com>
2024-01-03 11:35:43 +0100

    simplify test

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,35 +14,28 @@ def client():
     app = create_app({"TESTING": True}, token_generator)
 
     with app.test_client() as client:
-        yield client, token_generator
+        yield client
 
     token_generator.driver.quit()
 
 
 def test_get_new_token_should_return_status_code_ok(client):
-    flask_client, _ = client
-    response = flask_client.get("/new_token")
+    response = client.get("/new_token")
     assert response.status_code == 200
 
 
 def test_get_new_token_should_return_a_json(client):
-    flask_client, _ = client
-    response = flask_client.get("/new_token")
+    response = client.get("/new_token")
     try:
         json.loads(response.data)
     except ValueError as e:
         raise AssertionError(f"The response is not a valid JSON: {e}")
 
 
-@pytest.mark.parametrize(
-    "exception", [Exception("Test exception"), Exception("Another test exception")]
-)
-def test_get_new_token_with_exception_should_return_status_code_500(
-    client, mocker, exception
-):
+def test_get_new_token_with_exception_should_return_status_code_500(client, mocker):
     mocker.patch(
-        "src.token.TokenGenerator.TokenGenerator.get_token", side_effect=exception
+        "src.token.TokenGenerator.TokenGenerator.get_token",
+        side_effect=Exception("Test Exception"),
     )
-    flask_client, _ = client
-    response = flask_client.get("/new_token")
+    response = client.get("/new_token")
     assert response.status_code == 500
