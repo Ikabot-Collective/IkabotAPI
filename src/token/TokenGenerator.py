85a2bd39e87fb1f6ef5e2cf7b260e55202cd2872
johnvittu <61547860+johnvittu@users.noreply.github.com>
2024-01-04 12:02:42 +0200

    Reload Selenium upon generation
    Reloading Selenium with randomized values when a new token is generated to maximize the diversity of the token. Unique identity is generated every time, increases generation time to 5-6 seconds.

--- src/token/TokenGenerator.py
+++ src/token/TokenGenerator.py
@@ -1,5 +1,6 @@
 import os
-
+import fake_useragent
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,17 +25,15 @@ class TokenGenerator:
         """
         Initialize TokenGenerator.
 
-        - Initializes the Selenium WebDriver with a headless Chrome browser.
         - Sets the path to the HTML file used for token generation.
         """
         current_directory = os.path.dirname(os.path.abspath(__file__))
         self.html_file_path = f"file:///{current_directory}/token.html"
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        self.driver = webdriver.Chrome(options=options)
 
     def get_token(self):
         """
+        Reload Selenium upon token generation for a unique identity.
+        
         Get a token from the HTML file.
 
         Waits for the presence of a specific div element in the HTML file and returns its text content as the token.
@@ -42,10 +41,22 @@ class TokenGenerator:
         Returns:
         - str: The generated token.
         """
-        self.driver.get(self.html_file_path)
+        user_agent = fake_useragent.UserAgent().random
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--disable-blink-features=AutomationControlled") 
+        options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+        options.add_experimental_option("useAutomationExtension", False) 
+        
+        driver = webdriver.Chrome(options=options)
+        driver.set_window_size(random.randint(800, 1200), random.randint(600, 900))
+        driver.set_window_position(random.randint(0, 800), random.randint(0, 600))
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        
+        driver.get(self.html_file_path)
 
-        div = WebDriverWait(self.driver, 10).until(
+        div = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//body/div[1]"))
         )
-
         return div.text
