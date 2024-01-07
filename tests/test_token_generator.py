8320d741d91dbe598be093a226e77b1e47c8ff5e
Elie TAILLARD <54487782+ElieTaillard@users.noreply.github.com>
2024-01-07 19:18:56 +0100

    Revert "Playwright async"

---
+++ tests/test_token_generator.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.token.TokenGenerator import TokenGenerator
+
+
+@pytest.fixture
+def token_generator():
+    generator = TokenGenerator()
+    yield generator
+    generator.driver.quit()
+
+
+def test_token_not_empty(token_generator):
+    token = token_generator.get_token()
+    assert token is not None, "Token should not be None"
+    assert token != "", "Token should not be an empty string"
+
+
+def test_tokens_are_unique(token_generator):
+    tokens = [token_generator.get_token() for _ in range(5)]
+    assert len(set(tokens)) == len(tokens), "Tokens should be unique"
+
+
+def test_token_characteristics(token_generator):
+    token = token_generator.get_token()
+    assert isinstance(token, str), "Token should be a string"
+    assert " " not in token, "Token should not contain any whitespace"
+    assert any(c.isupper() for c in token), "The string must contain uppercase letters."
+    assert any(c.islower() for c in token), "The string must contain lowercase letters."
+    assert any(c.isdigit() for c in token), "The string must contain digits."
