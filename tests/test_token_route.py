6e6ce05591509fd72b38fe8c9cdcd53a65f95aa6
ElieTaillard <taillardelie@gmail.com>
2024-02-01 17:17:17 +0100

    Endpoints changes

--- tests/test_token_route.py
+++ tests/test_token_route.py
@@ -14,25 +14,25 @@ def client():
         yield client
 
 
-def test_get_new_token_should_return_status_code_ok(client):
-    response = client.get("/new_token")
+def test_get_token_should_return_status_code_ok(client):
+    response = client.get("/token")
     assert response.status_code == 200
 
 
-def test_get_new_token_should_return_a_json(client):
-    response = client.get("/new_token")
+def test_get_token_should_return_a_json(client):
+    response = client.get("/token")
     try:
         json.loads(response.data)
     except ValueError as e:
         raise AssertionError(f"The response is not a valid JSON: {e}")
 
 
-def test_get_new_token_with_exception_should_return_status_code_500(
+def test_get_token_with_exception_should_return_status_code_500(
     client, mocker: MockerFixture
 ):
     mocker.patch(
         "apps.token.TokenGenerator.get_token",
         side_effect=Exception("Test Exception"),
     )
-    response = client.get("/new_token")
+    response = client.get("/token")
     assert response.status_code == 500
