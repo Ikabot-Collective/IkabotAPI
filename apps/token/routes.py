cc02ae9c78124fc2b19b7fa3cff043a7414fdba9
Elie TAILLARD <54487782+ElieTaillard@users.noreply.github.com>
2024-02-18 15:41:50 +0100

    Fix information exposure through exceptions (#19)

--- apps/token/routes.py
+++ apps/token/routes.py
@@ -14,13 +14,13 @@ def token_route():
         token = token_generator.get_token()
         logger.info("Token generated in %s seconds" % (time.time() - start_time))
         return jsonify(token), 200
-    except Exception as e:
-        logger.error(e)
+    except Exception:
+        logger.exception("An error occurred during the token generation")
         return (
             jsonify(
                 {
                     "status": "error",
-                    "message": f"An error occurred during the token generation: {e}",
+                    "message": "An error occurred during the token generation",
                 }
             ),
             500,
