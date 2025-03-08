9ffe99582d34589faa3494fc0077dc6792222cd8
ikagod <65701886+ikagod@users.noreply.github.com>
2025-03-08 22:06:13 +0100

    Add index page

--- apps/home/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-from apps.home import blueprint
-
-
-@blueprint.route("/")
-def home():
-    return render_template("home/index.html")
--- settings.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-LOGS_WEBHOOK_URL = os.getenv("LOGS_WEBHOOK_URL")
