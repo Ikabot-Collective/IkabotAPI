00db287291810b8321b7118793052de53398758a
Elie TAILLARD <54487782+ElieTaillard@users.noreply.github.com>
2025-09-07 23:26:57 +0200

    Update test_decaptcha_pirate_route.py

--- tests/test_decaptcha_pirate_route.py
+++ tests/test_decaptcha_pirate_route.py
@@ -11,10 +11,10 @@ def test_decaptcha_without_data_should_return_422(client: TestClient):
     assert response.status_code == 422
 
 
-def test_decaptcha_piracy_with_valid_image_should_return_coordinates(
+def test_decaptcha_piracy_with_valid_image_should_return_the_right_string(
     client: TestClient,
 ):
-    """Test pirate captcha solving with valid images"""
+    """Test pirate captcha solving with valid images returns correct strings"""
     current_directory = os.path.dirname(__file__)
 
     # Case 1
@@ -24,9 +24,8 @@ def test_decaptcha_piracy_with_valid_image_should_return_coordinates(
         response = client.post("/v1/decaptcha/pirate", files=files)
 
     assert response.status_code == 200
-    # Returns the result string
     result = response.json()
-    assert isinstance(result, str)
+    assert result == "QKB24JC"
 
     # Case 2
     file_path = os.path.join(current_directory, "img", "pirate2.png")
@@ -35,12 +34,11 @@ def test_decaptcha_piracy_with_valid_image_should_return_coordinates(
         response = client.post("/v1/decaptcha/pirate", files=files)
 
     assert response.status_code == 200
-    # Returns the result string
     result = response.json()
-    assert isinstance(result, str)
+    assert result == "DEVL5KA"
 
 
-def test_decaptcha_piracy_with_invalid_size_should_return_500(client: TestClient):
+def test_decaptcha_piracy_with_invalid_size_should_return_status_code_500(client: TestClient):
     """Test pirate captcha with invalid image size"""
     current_directory = os.path.dirname(__file__)
     file_path = os.path.join(current_directory, "img", "pirate_invalid_size.png")
